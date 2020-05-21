3a3f82751c0f036a9f18cd602d2261fcd1d024a0
Griffith88 <dobrvn@gmail.com>
2020-05-21 15:13:09 +0300

    new programm on week3_1 simple

--- week3/test_class_inheritance.py
+++ week3/test_class_inheritance.py
@@ -4,9 +4,12 @@ class Pet:
         self.weight = weight
 
 class Cat(Pet):
-    def __init__(self,name,shmups=True):
-        self.name = name
+    def __init__(self,name,weight,shmups=True):
+        super().__init__(name, weight)
         self.shmups = shmups or []
 
     def say(self):
-        return '{} say mrmmrmrmrm'.format(self.name)
+        return '{} say mrmmrmrmrm with weight {}, is he SHMUPSIK PUPSIK??? Answer = {} '.format(self.name,self.weight,self.shmups)
+
+cat = Cat('Zhihar', '4kg')
+print(cat.say())
