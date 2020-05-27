baaba500118276000598e0fb925e758678c1c929
Griffith88 <dobrvn@gmail.com>
2020-05-27 10:15:00 +0300

    beta ver 1.2 need help

--- week4/solution.py
+++ week4/solution.py
@@ -1,30 +1,45 @@
-from tempfile import gettempdir
-
+import os.path
+import tempfile
 
 class File:
 
-    def __init__(self, file_path):
+    def __init__(self, file_path, value=None):
         self.file_path = file_path
+        self.value = value or ''
         with open(self.file_path, 'w') as f:
-            f.write('')
+            f.write(self.value)
 
     def __add__(self, other):
-        new_obj = File(self.read() + other.read())
+        new_obj = File(str.join(tempfile.gettempdir(), 'new_file'), self.value + other.value)
+        print(new_obj)
         return new_obj
+
+    def __str__(self):
+        return self.file_path
 
     def read(self):
         with open(self.file_path, 'r') as f:
-            print(f.read())
-        return f.read()
+            self.value = f.read()
+            return self.value
 
     def write(self, data):
         with open(self.file_path, 'w') as f:
             f.write(data)
             print(len(data))
+            return f.write(data)
 
-def main():
-    pass
+path_to_file = 'some_filename'
+file_obj = File(path_to_file)
+os.path.exists(path_to_file)
+file_obj.read()
+file_obj.write('some text')
+file_obj.read()
+file_obj.write('other text')
+file_obj.read()
+file_obj_1 = File(path_to_file + '_1')
+file_obj_2 = File(path_to_file + '_2')
+file_obj_1.write('line 1\n')
+file_obj_2.write('line 2\n')
+new_file_obj = file_obj_1 + file_obj_2
 
 
-if __name__ == '__main__':
-    main()
