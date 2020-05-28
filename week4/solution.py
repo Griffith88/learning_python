f7ae75b2577a7165d001d52660a9af614c671e2e
Griffith88 <dobrvn@gmail.com>
2020-05-28 11:08:30 +0300

    its working!

--- week4/solution.py
+++ week4/solution.py
@@ -8,15 +8,18 @@ class File:
         self.file_path = file_path
         self.value = value or ''
         if os.path.exists(self.file_path):
-            data = self.read()
+            open(self.file_path).close()
+        else:
             with open(self.file_path, 'w') as f:
-                f.write(data)
-        with open(self.file_path, 'w') as f:
-            f.write(self.value)
+                f.write(self.value)
 
     def __add__(self, other):
-        file_dir = os.path.join(tempfile.gettempdir(), 'new_obj')
-        new_obj = File(file_dir, self.read() + other.read())
+        file_dir = tempfile.NamedTemporaryFile(delete=False).name
+        #fd, file_dir = tempfile.mkstemp()
+        print(f'Впишем эти значения {self.read()} и {other.read()}')
+        new_obj = File(file_dir)
+        with open(file_dir, 'w') as f:
+            f.write(self.read() + other.read())
         return new_obj
 
     def __str__(self):
@@ -47,20 +50,20 @@ class File:
             f.write(data)
             print(len(data))
 
-path_to_file = 'some_filename'
-file_obj = File(path_to_file)
-os.path.exists(path_to_file)
-file_obj.read()
-file_obj.write('some text')
-file_obj.read()
-file_obj.write('other text')
-file_obj.read()
-file_obj_1 = File(path_to_file + '_1')
-file_obj_2 = File(path_to_file + '_2')
-file_obj_1.write('line 1\n')
-file_obj_2.write('line 2\n')
-new_file_obj = file_obj_1 + file_obj_2
-print(new_file_obj)
-print(isinstance(new_file_obj, File))
-for line in new_file_obj:
-    print(ascii(line))
+# path_to_file = 'some_filename'
+# file_obj = File(path_to_file)
+# os.path.exists(path_to_file)
+# file_obj.read()
+# file_obj.write('some text')
+# file_obj.read()
+# file_obj.write('other text')
+# file_obj.read()
+# file_obj_1 = File(path_to_file + '_1')
+# file_obj_2 = File(path_to_file + '_2')
+# file_obj_1.write('line 1\n')
+# file_obj_2.write('line 2\n')
+# new_file_obj = file_obj_1 + file_obj_2
+# print(new_file_obj)
+# print(isinstance(new_file_obj, File))
+# for line in new_file_obj:
+#     print(ascii(line))
