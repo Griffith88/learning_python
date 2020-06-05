1161b3e069c467e319b386be79aa21d2db5052fb
Griffith88 <dobrvn@gmail.com>
2020-06-05 22:26:14 +0300

    client. add put method v1.1

--- week5/client.py
+++ week5/client.py
@@ -1,24 +1,36 @@
 import socket
-# import time
-#
-# class Client:
-#     def __init__(self, ip, port, timeout=None):
-#         self.ip = str(ip)
-#         self.port = int(port)
-#         self.timeout = timeout or None
-#
-#         with socket.create_connection((self.ip, self.port)) as sock:
-#             self.sock = sock
-#             pass
-#
-#     def put(self, metric, value, timestamp=None):
-#         data_map = (metric, value, timestamp)
-#         self.sock.sendall('data'.encode('utf8'))
-#
-#     def get(self):
-#         pass
-
-sock = socket.create_connection(("127.0.0.1", 10001))
-sock.sendall('daasdasdas'.encode('utf8'))
+import time
 
 
+class ClientError(Exception):
+    def __init__(self,text):
+        self.text = text
+
+
+
+class Client:
+    def __init__(self, ip, port, timeout=None):
+        self.ip = str(ip)
+        self.port = int(port)
+        self.timeout = timeout or None
+        try:
+            self.sock = socket.create_connection((self.ip, self.port), self.timeout)
+        except:
+            raise ClientError('Server is not running')
+
+    def put(self, metric, value, timestamp=None):
+        self.timestamp = timestamp or int(time.time())
+        b_data_map = bytes(f'put {metric} {value} {self.timestamp}\n', 'utf8')
+        try:
+            self.sock.sendall(b_data_map)
+        except:
+            raise ClientError('No connection to server')
+
+    def get(self):
+        pass
+
+
+client = Client('127.0.0.1', 8888, timeout=30)
+client.put('palm.cpu', 23.7, 1150864247)
+client.put("palm.cpu", 0.5, timestamp=1150864248)
+client.put("eardrum.memory", 4200000)
