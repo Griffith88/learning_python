91c8b3f12a35f8e3fc19f7e875c32c783cb779ed
Griffith88 <dobrvn@gmail.com>
2020-06-11 22:06:17 +0300

    week 5 done

--- week5/client.py
+++ week5/client.py
@@ -1,11 +1,10 @@
 import socket
 import time
-
+import operator
 
 class ClientError(Exception):
-    def __init__(self,text):
+    def __init__(self, text):
         self.text = text
-
 
 
 class Client:
@@ -25,12 +24,58 @@ class Client:
             self.sock.sendall(b_data_map)
         except:
             raise ClientError('No connection to server')
+        b_answer = bytes('', 'utf8')
+        try:
+            while not b_answer.endswith(bytes('\n\n', 'utf8')):
+                b_answer += self.sock.recv(1024)
+        except:
+            raise ClientError('Can\'t recieve data')
+        if not b_answer.startswith(bytes('ok', 'utf8')):
+            raise ClientError('Invalid Answer')
+        answer = b_answer.decode('utf-8')
+        return answer
 
-    def get(self):
-        pass
+    def get(self, key):
+        b_request = bytes(f'get {key}\n', 'utf8')
+        try:
+            self.sock.sendall(b_request)
+        except:
+            raise ClientError('No connection to server')
+        b_answer = bytes('', 'utf8')
+        try:
+            while not b_answer.endswith(bytes('\n\n', 'utf8')):
+                b_answer += self.sock.recv(1024)
+        except:
+            raise ClientError('Can\'t recieve data')
+        if not b_answer.startswith(bytes('ok\n', 'utf8')):
+            raise ClientError('Invalid Answer')
+        # elif b_answer.startswith(bytes('error\n', 'utf8')):
+        #     raise ClientError('Error Requesting')
+        answer = b_answer.decode('utf-8')
+        try:
+            status, data_answer = answer.split('\n', 1)
+        except:
+            raise ClientError('Invalid extract')
+        if status == 'error':
+            raise ClientError('Error requesting')
+        data_answer = data_answer.strip()
+        data_dict = {}
+        if data_answer == '':
+            return data_dict
+        for row in data_answer.split('\n'):
+            try:
+                key, value, timestamp = row.split()
+            except:
+                raise ClientError('Error extract')
+            if key not in data_dict:
+                data_dict[key] = []
+            try:
+                data_dict[key].append((int(timestamp), float(value)))
+            except:
+                raise ClientError('Invalid values')
+        # s_data_dict = sorted(data_dict.items(), key=lambda x: x[1][0], reverse=False)
+        s_data_dict = {item[0]: sorted(item[1]) for item in data_dict.items()}
+        return dict(s_data_dict)
 
-
-client = Client('127.0.0.1', 8888, timeout=30)
-client.put('palm.cpu', 23.7, 1150864247)
-client.put("palm.cpu", 0.5, timestamp=1150864248)
-client.put("eardrum.memory", 4200000)
+# client = Client('127.0.0.1', 8888, timeout=30)
+# print(client.get('*'))
--- week5/server.py
+++ week5/server.py
@@ -1,12 +1,28 @@
+# реализация сервера для тестирования метода get по заданию - Клиент для отправки метрик
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import socket
 
-sock=socket.socket()
-sock.bind(("127.0.0.1", 8888))
-sock.listen()
+sock = socket.socket()
+sock.bind(('127.0.0.1', 8888))
+sock.listen(1)
 conn, addr = sock.accept()
+
+print('Соединение установлено:', addr)
+
+# переменная response хранит строку возвращаемую сервером, если вам для
+# тестирования клиента необходим другой ответ, измените ее
+response = b'ok\npalm.cpu 10.5 81\neardrum.cpu 15.3 12\npalm.cpu 80.5 4\npalm.mem 10235 15\neardrum.cpu 11.3 12\neardrum.cpu 19.3 16\n\n'
+
 while True:
     data = conn.recv(1024)
     if not data:
         break
-    print(data.decode("utf8"))
+    request = data.decode('utf-8')
+    print(f'Получен запрос: {ascii(request)}')
+    print(f'Отправлен ответ {ascii(response.decode("utf-8"))}')
+    conn.send(response)
+
+conn.close()
 
